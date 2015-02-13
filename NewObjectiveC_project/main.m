a6e3d3be3bcab76de966d231238a0f2e516cf630
Hugo <178764216@qq.com>
2015-02-14 07:46:51 +0800

    create array_2015-2-14

--- NewObjectiveC_project/main.m
+++ NewObjectiveC_project/main.m
@@ -8,11 +8,19 @@
 
 #import <Foundation/Foundation.h>
 #import "Student.h"
+
+
+#pragma mark 数组对象的建立
+void arrayCreate(){
+    NSArray *array=[[[NSArray alloc] init] autorelease];
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         Student *stu1=[Student student];
         stu1.age=12;
         NSLog(@"student1 age= %i",stu1.age);
+    
     }
     return 0;
 }
